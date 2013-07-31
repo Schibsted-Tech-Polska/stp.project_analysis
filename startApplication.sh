97d56a3740fa8b00717b82cdf97d6322f126d55e
tomekl007 <tomekl007@gmail.com>
2013-07-31 10:52:53 +0200

    add env to path

--- startApplication.sh
+++ startApplication.sh
@@ -1,5 +1,5 @@
 
 export PATH="$PWD"/env/sonar-runner-2.2.2/bin:$PATH
 #start sonar server, will be listen on port 9000
-sh sonar-3.6.1/bin/macosx-universal-64/sonar.sh start
+sh env/sonar-3.6.1/bin/macosx-universal-64/sonar.sh start
 node ./app.js
