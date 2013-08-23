fffa153f54887970139387afb0ccd275673b3be7
Tomasz Lelek <tomekl007@gmail.com>
2013-08-23 19:13:19 +0200

    Delete test.js

--- tests/test.js
+++
@@ -1,8 +0,0 @@
-var exec = require('child_process').exec;
-
-describe('response', function(){
-	it('should return redirecting to ...', function(){
-		exec("curl -X POST -H 'Content-Type: application/json' -d '{'link': 'https%3A%2F%2Fgithub.com%2Ftomekl007%2Fmulitmethods_like_clojure%0A',
-		'targetLanguage':'java'}' http://localhost:3000/inputForm");
-	})
-})
